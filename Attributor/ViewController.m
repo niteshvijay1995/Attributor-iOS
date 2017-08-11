0e7e437943cea6ecc6828785db7b3ac1e6099346
Nitesh Vijay <niteshvijay1995@gmail.com>
2017-08-11 13:05:10 +0530

    Add outline to outline button

--- Attributor/ViewController.m
+++ Attributor/ViewController.m
@@ -10,10 +10,20 @@
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextView *textViewBody;
+@property (weak, nonatomic) IBOutlet UIButton *outlineButton;
 
 @end
 
 @implementation ViewController
+
+-(void) viewDidLoad
+{
+    [super viewDidLoad];
+    NSMutableAttributedString *title = [[NSMutableAttributedString alloc] initWithString: self.outlineButton.currentTitle];
+    [title setAttributes:@{NSStrokeWidthAttributeName : @3,
+                           NSStrokeColorAttributeName : self.outlineButton.tintColor} range:NSMakeRange(0, [title length])];
+    [self.outlineButton setAttributedTitle:title forState:UIControlStateNormal];
+}
 
 - (IBAction)changeBackgroudColorOfSelectedTextTo:(UIButton *)sender {
     [self.textViewBody.textStorage addAttribute:NSForegroundColorAttributeName value:sender.backgroundColor range:self.textViewBody.selectedRange];
